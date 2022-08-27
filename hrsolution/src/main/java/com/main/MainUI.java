0ac6427b334698d1edd000194f8e965a70add2ed
soyeon_computer <thdus_17@naver.com>
2022-08-27 12:22:38 +0900

    2022-08-27 정소연 - 로그인 기능 수정

--- hrsolution/src/main/java/com/main/MainUI.java
+++ hrsolution/src/main/java/com/main/MainUI.java
@@ -5,17 +5,28 @@ import java.io.InputStreamReader;
 
 import com.util.DBConn;
 
+import employee.EmployeeDAO;
+import employee.EmployeeDAOImpl;
+import employee.EmployeeDTO;
+import employee.EmployeeUI;
+
 // 시작메뉴, 메인메뉴 UI
 // UI 꾸미기 필요
 public class MainUI {
 	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-	private Login login = new Login();
-	private LoginProcess loginprocess = new LoginProcess(login);
+	// private Login login = new Login();
+	// private LoginProcess loginprocess = new LoginProcess(login);
+	private EmployeeDAO empdao = new EmployeeDAOImpl();
+	private EmployeeUI employeemenu = new EmployeeUI();
+	// private EmployeeDAO empDAO = new EmployeeUI();
+	private EmployeeDTO loginEmp = null;
+	
+	
 
 	public void menu() {
 		while(true) {
-			if(login.login() == null) {
+			if(loginEmp == null) {
 				startmenu();
 			} else {
 				mainmenu();
@@ -45,29 +56,60 @@ public class MainUI {
 		}
 		
 		switch(ch) {
-		case 1: loginprocess.loginmenu(); break;
+		case 1: login();  break;
 		}
+	}
+	
+	public void login() {
+		System.out.println("로그인 메뉴 실행");
+		String id, pwd;
+
+		try {
+			System.out.println("사번 ? ");
+			id = br.readLine();
+
+			System.out.println("패스워드 ? ");
+			pwd = br.readLine();
+
+			EmployeeDTO empdto = empdao.readMember(id);
+
+			if (empdto == null) {
+				System.out.println("사번이 존재하지 않습니다. ");
+				return;
+			} else if (!empdto.getPwd().equals(pwd)) {
+				System.out.println("패스워드가 일치하지 않습니다. ");
+				return;
+			}
+			
+			loginEmp = empdto;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 	public void mainmenu() {
 		int ch;
 		
 		try {
-			System.out.println("\n[사용자: "+login.login().getId()+"] 님");
+			System.out.println("\n[사용자: "+loginEmp.getId()+"] 님");
 			do {
 				System.out.println("1.사원관리 2.급여관리 3.채용공고 4.종합정보 5.평가관리 6.근태관리 7.로그아웃");
 				ch = Integer.parseInt(br.readLine());
 			} while(ch<1 || ch>7);
 			
 			switch(ch) {
-			case 1: break;
+			case 1: 
+				employeemenu.employeemenu(loginEmp);
+				break;
 			case 2: break;
 			case 3: break;
 			case 4: break;
 			case 5: break;
 			case 6: break;
 			case 7: 
-				login.logout(); 
+				loginEmp = null;
 				System.out.println(); 
 				break;
 			}
--- hrsolution/src/main/java/employee/EmployeeUI.java
+++ hrsolution/src/main/java/employee/EmployeeUI.java
@@ -1,5 +1,10 @@
 package employee;
 
 public class EmployeeUI {
+	
+	public void employeemenu(EmployeeDTO empdto) {
+		System.out.println("사원 관리 메뉴 실행");
+		System.out.println(empdto.getId()+" 님");
+	}
 
 }
