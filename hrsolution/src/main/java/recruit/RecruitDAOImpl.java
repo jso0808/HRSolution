f521323ebb7da4d6ed4aaa641eee9efe7b68c38a
jang <jang@DESKTOP-1QOKUOI>
2022-08-30 15:35:41 +0900

    채용공고테이블내용만 ...

--- hrsolution/src/main/java/recruit/RecruitDAOImpl.java
+++ hrsolution/src/main/java/recruit/RecruitDAOImpl.java
@@ -1,5 +1,317 @@
 package recruit;
 
-public class RecruitDAOImpl {
+import java.sql.SQLException;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLDataException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+
+
+import com.util.DBConn;
+
+
+
+
+
+
+public  class RecruitDAOImpl implements RecruitDAO{
+	private Connection conn = DBConn.getConnection();
+	@Override
+	public int inserRecruit(RecruitDTO dto) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+		try {
+			sql = "INSERT INTO posting(posNo,posNum,pos,posStart,posEnd,posMeans,PosPro,deptNo,id,posTitle) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getPosNo());
+			pstmt.setInt(2, dto.getPosNum());
+			pstmt.setString(3, dto.getPos());
+			pstmt.setString(4, dto.getPosStart());
+			pstmt.setString(5, dto.getPosEnd());
+			pstmt.setString(6, dto.getPosMeans());
+			pstmt.setString(7, dto.getPosPro());
+			pstmt.setString(8, dto.getDeptNo());
+			pstmt.setString(9, dto.getId());
+			pstmt.setString(10, dto.getPosTitle());
+			
+			result += pstmt.executeUpdate();
+		
+		} catch (SQLIntegrityConstraintViolationException e) {
+			if(e.getErrorCode()==1400){ 
+				System.out.println("필수 입력사항을 입력하지 않았습니다.");
+			} else {
+				System.out.println(e.toString());
+			}
+			throw e;
+		} catch (SQLDataException e) {
+			
+			if(e.getErrorCode()==1840 || e.getErrorCode()==1861) {
+				System.out.println("날짜 입력 형식 오류입니다.");
+			} else {
+				System.out.println(e.toString());
+			}
+			throw e;			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
+		
+	}
+
+	@Override
+	public int updateRecruit(RecruitDTO dto) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			sql = "UPDATE  posting SET posNum=?, pos=?, posStart=?, posEnd=?, posMeans=?, PosPro=?, deptNo=?, id=?, posTitle=? WHERE posNo=?";
+
+			pstmt = conn.prepareStatement(sql);
+		
+			    pstmt.setInt(1, dto.getPosNum());
+			    pstmt.setString(2, dto.getPos());
+			    pstmt.setString(3, dto.getPosStart());
+			    pstmt.setString(4, dto.getPosEnd());
+			    pstmt.setString(5, dto.getPosMeans());
+			    pstmt.setString(6, dto.getPosPro());
+			    pstmt.setString(7, dto.getDeptNo());
+			    pstmt.setString(8, dto.getId());
+			    pstmt.setString(9, dto.getPosTitle());
+				pstmt.setString(10, dto.getPosNo());
+
+			
+			
+
+			result += pstmt.executeUpdate();
+
+		} catch (SQLIntegrityConstraintViolationException e) {
+			if(e.getErrorCode()==1400){ 
+				System.out.println("필수 입력사항을 입력하지 않았습니다.");
+			} else {
+				System.out.println(e.toString());
+			}
+			throw e;
+		} catch (SQLDataException e) {
+			
+			if(e.getErrorCode()==1840 || e.getErrorCode()==1861) {
+				System.out.println("날짜 입력 형식 오류입니다.");
+			} else {
+				System.out.println(e.toString());
+			}
+			throw e;			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
+	}
+
+	@Override
+	public int deleteRecruit(String posNo) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			sql = "DELETE FROM  posting WHERE posNo=?";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, posNo);
+			
+
+			result = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
+	}
+
+	@Override
+	public RecruitDTO readRecruit(String posNo) {
+		RecruitDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "SELECT posNo,posNum,pos,posStart,posEnd,posMeans,PosPro,deptNo,id,posTitle From Posting WHERE posNo=?";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, posNo);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new RecruitDTO();
+
+				dto.setPosNo(rs.getString("posNo"));
+				dto.setPosNum(rs.getInt("posNum"));
+				dto.setPos(rs.getString("pos"));
+				dto.setPosStart(rs.getString("posStart"));
+				dto.setPosEnd(rs.getString("posEnd"));
+				dto.setPosMeans(rs.getString("posMeans"));
+				dto.setPosPro(rs.getString("posPro"));
+				dto.setDeptNo(rs.getString("deptNo"));
+				dto.setId(rs.getString("id"));
+				dto.setPosTitle(rs.getString("posTitle"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+
+		return dto;
+	
+	}
+
+	@Override
+	public List<RecruitDTO> listRecruit() {
+		List<RecruitDTO> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "SELECT posNo,posNum,pos,posStart,posEnd,posMeans,PosPro,deptNo,id,posTitle From Posting";
+
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				RecruitDTO dto = new RecruitDTO();
+
+				dto.setPosNo(rs.getString("posNo"));
+				dto.setPosNum(rs.getInt("posNum"));
+				dto.setPos(rs.getString("pos"));
+				dto.setPosStart(rs.getString("posStart"));
+				dto.setPosEnd(rs.getString("posEnd"));
+				dto.setPosMeans(rs.getString("posMeans"));
+				dto.setPosPro(rs.getString("posPro"));
+				dto.setDeptNo(rs.getString("deptNo"));
+				dto.setId(rs.getString("id"));
+				dto.setPosTitle(rs.getString("posTitle"));
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+
+		return list;
+	}
+
+	@Override
+	public List<RecruitDTO> listRecruit(String id) {
+		List<RecruitDTO> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = "SELECT posNo,posNum,pos,posStart,posEnd,posMeans,PosPro,deptNo,id,posTitle From Posting where posting.id=? ";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				RecruitDTO dto = new RecruitDTO();
+
+				dto.setPosNo(rs.getString("posNo"));
+				dto.setPosNum(rs.getInt("posNum"));
+				dto.setPos(rs.getString("pos"));
+				dto.setPosStart(rs.getString("posStart"));
+				dto.setPosEnd(rs.getString("posEnd"));
+				dto.setPosMeans(rs.getString("posMeans"));
+				dto.setPosPro(rs.getString("posPro"));
+				dto.setDeptNo(rs.getString("deptNo"));
+				dto.setId(rs.getString("id"));
+				dto.setPosTitle(rs.getString("posTitle"));
+
+				list.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+
+		return list;
+	}
+	
+	
+
+	
 
 }
--- hrsolution/src/main/java/recruit/RecruitDTO.java
+++ hrsolution/src/main/java/recruit/RecruitDTO.java
@@ -1,5 +1,199 @@
 package recruit;
 
 public class RecruitDTO {
+private String posNo;
+private int PosNum;
+private String pos;
+private String PosStart;
+private String posEnd;
+private String PosMeans;
+private String PosPro;
+private String deptNo;
+private String id;
+private String posTitle;
 
+private String ApNo;
+private String ApName;
+private String ApBirth;
+private String ApDate;
+private String ApInterview;
+private String ApRoute;
+
+private String ApResult1;
+private String ApResult2;
+private String ApResult3;
+private String ApTel;
+
+private String evNo;
+
+private String evGrade1;
+private int evGrade2;
+private int evGrade3;
+private int evReason;
+
+private String idRe;
+private String ReNo;
+
+public String getIdRe() {
+	return idRe;
+}
+public void setIdRe(String idRe) {
+	this.idRe = idRe;
+}
+public String getReNo() {
+	return ReNo;
+}
+public void setReNo(String reNo) {
+	ReNo = reNo;
+}
+public String getEvNo() {
+	return evNo;
+}
+public void setEvNo(String evNo) {
+	this.evNo = evNo;
+}
+public String getEvGrade1() {
+	return evGrade1;
+}
+public void setEvGrade1(String evGrade1) {
+	this.evGrade1 = evGrade1;
+}
+public int getEvGrade2() {
+	return evGrade2;
+}
+public void setEvGrade2(int evGrade2) {
+	this.evGrade2 = evGrade2;
+}
+public int getEvGrade3() {
+	return evGrade3;
+}
+public void setEvGrade3(int evGrade3) {
+	this.evGrade3 = evGrade3;
+}
+public int getEvReason() {
+	return evReason;
+}
+public void setEvReason(int evReason) {
+	this.evReason = evReason;
+}
+public String getApNo() {
+	return ApNo;
+}
+public void setApNo(String apNo) {
+	ApNo = apNo;
+}
+public String getApName() {
+	return ApName;
+}
+public void setApName(String apName) {
+	ApName = apName;
+}
+public String getApBirth() {
+	return ApBirth;
+}
+public void setApBirth(String apBirth) {
+	ApBirth = apBirth;
+}
+public String getApDate() {
+	return ApDate;
+}
+public void setApDate(String apDate) {
+	ApDate = apDate;
+}
+public String getApInterview() {
+	return ApInterview;
+}
+public void setApInterview(String apInterview) {
+	ApInterview = apInterview;
+}
+public String getApRoute() {
+	return ApRoute;
+}
+public void setApRoute(String apRoute) {
+	ApRoute = apRoute;
+}
+public String getApResult1() {
+	return ApResult1;
+}
+public void setApResult1(String apResult1) {
+	ApResult1 = apResult1;
+}
+public String getApResult2() {
+	return ApResult2;
+}
+public void setApResult2(String apResult2) {
+	ApResult2 = apResult2;
+}
+public String getApResult3() {
+	return ApResult3;
+}
+public void setApResult3(String apResult3) {
+	ApResult3 = apResult3;
+}
+public String getApTel() {
+	return ApTel;
+}
+public void setApTel(String apTel) {
+	ApTel = apTel;
+}
+public String getPosNo() {
+	return posNo;
+}
+public void setPosNo(String posNo) {
+	this.posNo = posNo;
+}
+public int getPosNum() {
+	return PosNum;
+}
+public void setPosNum(int posNum) {
+	PosNum = posNum;
+}
+public String getPos() {
+	return pos;
+}
+public void setPos(String pos) {
+	this.pos = pos;
+}
+public String getPosStart() {
+	return PosStart;
+}
+public void setPosStart(String posStart) {
+	PosStart = posStart;
+}
+public String getPosEnd() {
+	return posEnd;
+}
+public void setPosEnd(String posEnd) {
+	this.posEnd = posEnd;
+}
+public String getPosMeans() {
+	return PosMeans;
+}
+public void setPosMeans(String posMeans) {
+	PosMeans = posMeans;
+}
+public String getPosPro() {
+	return PosPro;
+}
+public void setPosPro(String posPro) {
+	PosPro = posPro;
+}
+public String getDeptNo() {
+	return deptNo;
+}
+public void setDeptNo(String deptNo) {
+	this.deptNo = deptNo;
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getPosTitle() {
+	return posTitle;
+}
+public void setPosTitle(String posTitle) {
+	this.posTitle = posTitle;
+}
 }
